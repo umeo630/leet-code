ccaae056753faccd9de3af6cfa29476d2a1908f3
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2024-02-27 00:15:32 +0900

    fix

--- tessoku/B06.php
+++ tessoku/B06.php
@@ -6,20 +6,20 @@ while ($line = trim(fgets(STDIN))) {
 $results = explode(' ', $lines[1]);
 
 $win[0] = 0;
-$lost[0] = 0;
+$lose[0] = 0;
 $winCount = 0;
-$lostCount = 0;
+$loseCount = 0;
 foreach ($results as $key => $result) {
-  $result ? $winCount++ : $lostCount++;
-  $win[$key] = $winCount;
-  $lost[$key] = $lostCount;
+  intval($result) === 1 ? $winCount++ : $loseCount++;
+  $win[$key + 1] = $winCount;
+  $lose[$key + 1] = $loseCount;
 }
 
-$lineCount = $line[2];
+$lineCount = intval($lines[2]);
 
 for ($i = 3; $i < $lineCount + 3; $i++) {
-  list($start, $end) = explode(' ', $line[$i]);
-  $difference = ($lost[$end - 1] - $lost[$start - 1]) - ($win[$end - 1] - $win[$start - 1]);
+  list($start, $end) = explode(' ', $lines[$i]);
+  $difference = ($win[$end] - $win[$start - 1]) - ($lose[$end] - $lose[$start - 1]);
   if ($difference > 0) echo 'win' . "\n";
   if ($difference === 0) echo 'draw' . "\n";
   if ($difference < 0) echo 'lose' . "\n";
